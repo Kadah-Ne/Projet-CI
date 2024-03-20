ffc6f9f8bc689e4fba03f2218dceda7402d5d4f1
bobzore96 <francois.goudet@ecoles-epsi.net>
2024-03-20 15:26:04 +0100

    test

--- Wishcord/MainProject/tests.py
+++ Wishcord/MainProject/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from MainProject.models import User, Group
+import MainProject.dbFunctions as dbf
 
-# Create your tests here.
+
+class DbTestCase(TestCase) :
+    def setUp(self) -> None:
+        User.objects.create(username = "testerbob", isAdmin=False)
+        Group.objects.create( name = "groupeTest")
+    
+    def testAddUser2Groupe(self): 
+        user = User.objects.get(username="testerbob")
+        groupe = Group.objects.get(name="groupeTest")
+    
+        self.assertTrue(dbf.addToGroup(user, groupe))
+
+    
