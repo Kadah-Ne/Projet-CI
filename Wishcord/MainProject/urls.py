204daf5f117a0075c02d12951616a2c1a3dfe002
Kadah-Ne <mgouviaux@gmail.com>
2024-04-15 13:49:58 +0200

    a

--- Wishcord/MainProject/urls.py
+++ Wishcord/MainProject/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login", views.login, name="login"),
+    path("groupList", views.groupList, name="groupList"),
+    path("groupeView", views.groupView, name="groupView"),
+    path("groupManage", views.groupManage, name="groupManage"),
+    path("adminPage", views.adminPage, name="adminPage"),
 ]
