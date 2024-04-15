8cd23a11f628e9dcf24700a275339f636d3cfdaa
Kadah-Ne <mgouviaux@gmail.com>
2024-04-15 13:45:03 +0200

    aaa

--- Wishcord/MainProject/static/js/Agenda.js
+++
@@ -1,8 +0,0 @@
-function setEvents()
-{
-    //Fetch user events
-    var events = [{title : "Plantes à garder",start:"2023-03-07",end:"2023-03-27"}]
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {events:events});
-    calendar.render();
-}
--- Wishcord/MainProject/views.py
+++ Wishcord/MainProject/views.py
@@ -4,4 +4,18 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "index.html")
-        
+
+def login(request):
+    pass
+
+def groupList(request):
+    pass
+
+def groupView(request):
+    pass
+
+def groupManage(request):
+    pass
+
+def adminPage(request):
+    pass
