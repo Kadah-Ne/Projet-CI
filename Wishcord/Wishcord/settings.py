ca0de21f188146e4925dffc56eaef9ea8cfdf43c
Kadah-Ne <mgouviaux@gmail.com>
2024-04-17 01:42:50 +0200

    d

--- Wishcord/Wishcord/settings.py
+++ Wishcord/Wishcord/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'MainProject.apps.MainprojectConfig'
+    'MainProject.apps.MainprojectConfig',
+    'django_jenkins',
 ]
 
 MIDDLEWARE = [
